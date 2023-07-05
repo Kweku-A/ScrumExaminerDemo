5aa432ae9bc2ec055124d740b484980b1f35b60c
Kweku <thunderson3@gmail.com>
2023-07-05 22:33:29 +0200

    Remove fileHelper provider to utilities module

--- professionalsoftwaredeveloper/data/src/main/java/com/kweku/armah/psd/data/di/DataModule.kt
+++ professionalsoftwaredeveloper/data/src/main/java/com/kweku/armah/psd/data/di/DataModule.kt
@@ -1,12 +1,10 @@
 package com.kweku.armah.psd.data.di
 
 import com.google.gson.Gson
-import com.kweku.armah.core.utilities.FileHelper
-import com.kweku.armah.core.utilities.FileHelperImpl
-import com.kweku.armah.psd.data.repository.PsdQuestionsRepositoryImpl
-import com.kweku.armah.psd.data.repository.PsdQuizQuestionsRepositoryImpl
 import com.kweku.armah.core.domain.repository.QuestionsRepository
 import com.kweku.armah.core.domain.repository.QuizQuestionsRepository
+import com.kweku.armah.psd.data.repository.PsdQuestionsRepositoryImpl
+import com.kweku.armah.psd.data.repository.PsdQuizQuestionsRepositoryImpl
 import com.kweku.armah.psd.domain.ProfessionalScrumDeveloper
 import dagger.Module
 import dagger.Provides
@@ -17,12 +15,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 object DataModule {
-
-    @Singleton
-    @Provides
-    fun provideFileHelper(fileHelperImpl: FileHelperImpl): FileHelper {
-        return fileHelperImpl
-    }
 
     @Singleton
     @Provides
