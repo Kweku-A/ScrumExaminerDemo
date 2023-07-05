a1cd89920f2d0162beb962a10f68d343868620ae
Kweku <thunderson3@gmail.com>
2023-07-05 21:50:01 +0200

    Added domain for pspo

---
+++ professionalscrumproductowner/domain/build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+    kotlin("kapt")
+    alias(libs.plugins.hiltPlugin)
+}
+
+android {
+    namespace = "com.kweku.armah.pspo.domain"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:domain"))
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
---
+++ professionalscrumproductowner/domain/src/main/java/com/kweku/armah/pspo/domain/Annotations.kt
@@ -0,0 +1,7 @@
+package com.kweku.armah.pspo.domain
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ProfessionalScrumProductOwner
