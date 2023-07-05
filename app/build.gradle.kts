3009099be2abdbe2b3b57d10df7821066a769878
Kweku <thunderson3@gmail.com>
2023-07-05 15:33:33 +0200

    Remove common folder and content

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,6 @@ dependencies {
     implementation(project(":core:data"))
     implementation(project(":core:domain"))
     implementation(project(":core:utilities"))
-    implementation(project(":common:composables"))
     implementation(project(":core:resources"))
 }
 
