826d934d90bceff9d5170b628ef0ed6c7cec733b
KwekuArmah <karmah@aligntech.com>
2023-08-01 20:30:16 +0200

    Clear quiz table before adding new quiz

--- core/domain/src/main/java/com/kweku/armah/core/domain/usecase/GenerateQuizUseCase.kt
+++ core/domain/src/main/java/com/kweku/armah/core/domain/usecase/GenerateQuizUseCase.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GenerateQuizUseCase @Inject constructor(
+    private val deleteQuizUseCase: DeleteQuizUseCase,
     private val selectQuizQuestionsUseCase: SelectQuizQuestionsUseCase,
     private val saveSelectedQuizQuestionsUseCase: SaveSelectedQuizQuestionsUseCase,
 ) {
@@ -14,6 +15,7 @@ class GenerateQuizUseCase @Inject constructor(
         questionsRepository: QuestionsRepository,
         quizQuestionsRepository: QuizQuestionsRepository,
     ) = flow {
+        deleteQuizUseCase(quizQuestionsRepository)
         val questions = selectQuizQuestionsUseCase(questionsRepository = questionsRepository)
         saveSelectedQuizQuestionsUseCase(
             quizQuestionsRepository = quizQuestionsRepository,
