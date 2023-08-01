c1d049f7cb1df351019a4a374393ba6ce8ebfe29
KwekuArmah <karmah@aligntech.com>
2023-08-01 23:25:02 +0200

    Update Question.kt

--- core/domain/src/main/java/com/kweku/armah/core/domain/model/Question.kt
+++ core/domain/src/main/java/com/kweku/armah/core/domain/model/Question.kt
@@ -2,10 +2,10 @@ package com.kweku.armah.core.domain.model
 
 data class Question(
     val id: Int = 0,
-    val question: String,
-    val answers: List<Answer>,
-    val correctAnswers: List<Answer>,
-    val selectedAnswers: List<Answer>,
+    val question: String = "",
+    val answers: List<Answer> = emptyList(),
+    val correctAnswers: List<Answer> = emptyList(),
+    val selectedAnswers: List<Answer> = emptyList(),
 )
 
 data class Answer(val data: String = "")
