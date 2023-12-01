3c543399eaac0ebe1693ecfac2f969b976a93b47
Kweku <thunderson3@gmail.com>
2023-12-01 10:16:32 +0100

    Move QuestionsFileDataSourceTest to correct package

--- core/data/src/test/java/com/kweku/armah/core/data/repository/QuestionsFileDataSourceTest.kt
+++ core/data/src/test/java/com/kweku/armah/core/data/repository/QuestionsFileDataSourceTest.kt
@@ -1,7 +1,9 @@
-package com.kweku.armah.core.domain
+package com.kweku.armah.core.data.repository
 
 import com.kweku.armah.core.domain.repository.QuestionsFileDataSource
 import com.kweku.armah.core.domain.repository.fakes.FakeQuestionsFileDataSource
+import com.kweku.armah.core.utilities.files.FileHelper
+import com.kweku.armah.core.utilities.files.fake.FakeFileHelperImpl
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
@@ -10,15 +12,17 @@ import org.junit.Test
 class QuestionsFileDataSourceTest {
 
     private lateinit var sut: QuestionsFileDataSource
+    private lateinit var fileHelper: FileHelper
 
     @Before
     fun setup() {
-        sut = FakeQuestionsFileDataSource()
+        fileHelper = FakeFileHelperImpl()
+        sut = QuestionsFileDataSourceImpl(fileHelper = fileHelper)
     }
 
     @Test
     fun shouldReturnListOfQuestionsFromFile() = runBlocking {
-        val expected = FakeQuestionsFileDataSource.fakeListOfQuestions
+        val expected = FakeFileHelperImpl.fakeListOfQuestions
         val actual = sut.getQuestionsFromFileIntoDatabase(1)
         Assert.assertEquals(expected, actual)
     }
